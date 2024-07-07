98431300dacc7fee834538b2e27b450f24247cda
Srija Adhya <srijaadhya.11sc.2020@gmail.com>
2024-07-08 00:13:53 +0530

    feat: added responsiveness

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ const Home = () => {
 	return (
 		<main>
 			<Hero />
+			<hr className='hidden sm:block'/>
             <TimeWorking />
             <Tools/>
 			<ProblemSolved />
--- src/sections/TimeWorking.jsx
+++ src/sections/TimeWorking.jsx
@@ -3,23 +3,19 @@ import { SecondaryButton } from '../components'
 
 const TimeWorking = () => {
 	return (
-		<section className="flex container px-2 mx-auto mt-52">
+		<section className="flex container sm:px-2 mx-auto sm:mt-52">
 			<div className="flex gap-4 w-1/2 justify-center">
 				<img className="h-96 mt-20" src={image1} alt="women" />
 				<img className="h-96" src={image2} alt="group" />
 				<img className="h-96 mt-14" src={image3} alt="women" />
 			</div>
-			<div className="w-1/2 px-10">
-				<h1 className="text-[62px] leading-[80px] font-extrabold">
+			<div className="sm:w-1/2 sm:px-10 px-7">
+				<h1 className="sm:text-6xl text-5xl font-bold sm:leading-[80px] sm:font-extrabold">
 					Enjoy your time
 					<br />
 					working
 				</h1>
-				<p className="text-gray-500 mt-8 leading-7">
-					Etiam condimentum duis molestie malesuada volutpat pellentesque sed. <br />
-					Ornare suspendisse ut ac neque lobortis sed tincidunt. Mi tempus quis <br />
-					massa tellus imperdiet aenean nulla id.
-				</p>
+				<p className="text-gray-500 mt-8 text-xl leading-7">Etiam condimentum duis molestie malesuada volutpat pellentesque sed. Ornare suspendisse ut ac neque lobortis sed tincidunt. Mi tempus quis massa tellus imperdiet aenean nulla id.</p>
 				<div className="mt-7">
 					<SecondaryButton arrow color="blue">
 						See how it helped others
