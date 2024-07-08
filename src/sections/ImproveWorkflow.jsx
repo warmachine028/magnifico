dd6bb9b99bac25d3c76a596b1d9d3c01bf1ef53b
Srija Adhya <srijaadhya.11sc.2020@gmail.com>
2024-07-08 11:34:37 +0530

    feat: responsive workflow section

--- src/sections/ImproveWorkflow.jsx
+++ src/sections/ImproveWorkflow.jsx
@@ -3,10 +3,10 @@ import { SecondaryButton } from "../components";
 
 const ImproveWorkflow = () => {
   return (
-		<div className="container px-2 mx-auto mt-52 gap-72 flex flex-row">
-			<div className="w-1/2">
-				<h1 className="text-6xl leading-[80px] font-extrabold  my-5">Improve workflow</h1>
-				<div className="flex w-full">
+		<div section id="improveworkflow" className="container sm:px-2 mx-auto sm:mt-52 mt-20 sm:gap-72 flex sm:flex-row flex-col px-5">
+			<div className="sm:w-1/2 w-full">
+				<h1 className="sm:text-6xl text-5xl font-bold sm:leading-[80px] sm:font-extrabold  sm:my-5 my-4">Improve workflow</h1>
+				<div className="flex sm:w-full my-7">
 					<button className="border rounded-l-lg flex items-center justify-center p-3 w-1/3 px-16 bg-slate-100">Research</button>
 					<button className="border flex items-center justify-center p-3 w-1/3">Plan</button>
 					<button className="border rounded-r-lg flex items-center justify-center p-3 w-1/3">Design</button>
@@ -14,14 +14,14 @@ const ImproveWorkflow = () => {
 				<div>
 					<p className="text-gray-500 mt-8 leading-7">Egestas fringilla aliquam leo, habitasse arcu varius lorem elit. Neque pellentesque donec et tellus ac varius tortor, bibendum. Nulla felis ac turpis at amet. Purus malesuada placerat arcu at enim elit in accumsan.</p>
 				</div>
-				<div className="flex gap-4 mt-7">
+				<div className="flex my-10">
 					<SecondaryButton arrow color="blue">
 						Check the tools
 					</SecondaryButton>
 				</div>
 			</div>
-			<div className="w-1/2">
-				<img src={Video2}></img>
+			<div className="sm:w-1/2 w-full justify-center items-center">
+				<img src={Video2} />
 			</div>
 		</div>
   )
