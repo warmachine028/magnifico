db9d444db99f77776e836de0d237fd210714241f
Srija Adhya <srijaadhya.11sc.2020@gmail.com>
2024-07-11 01:29:10 +0530

    feat: payment section

--- src/components/CreditCardForm.jsx
+++ src/components/CreditCardForm.jsx
@@ -1,5 +1,5 @@
 import { DinersClub, MasterCard, Visa } from '../assets'
-import { PiPiggyBankFill } from 'react-icons/pi'
+import { PiInfoBold, PiInfoLight, PiPiggyBankFill } from 'react-icons/pi'
 import { BsInfoCircleFill } from 'react-icons/bs'
 import { MdLock } from 'react-icons/md'
 import { IoMdCard } from 'react-icons/io'
@@ -25,7 +25,7 @@ const CreditCardForm = () => {
 						<input className="h-14 w-1/2 rounded-2xl px-4 ring-1 ring-gray-400 after:ml-0.5 after:text-red-500 placeholder:after:content-['*']" placeholder="Name on card" required autoComplete="cc-name" type="text" />
 						<div className="relative w-1/2">
 							<div className="pointer-events-none absolute inset-y-0 start-0 flex items-center ps-3.5">
-								<IoMdCard className="text-gray-600" />
+								<IoMdCard className="size-5 text-gray-600" />
 							</div>
 							<input type="email" className="h-14 w-full rounded-2xl px-4 ps-10 text-gray-400 ring-1 ring-gray-400" placeholder="Card Number" autoComplete="cc-number" required />
 						</div>
@@ -47,22 +47,22 @@ const CreditCardForm = () => {
 					<Link to="#" className="text-sm text-gray-700 underline decoration-gray-400">
 						You will be charged in AED
 					</Link>
-					<BsInfoCircleFill color="gray" className="cursor-pointer" />
+					<PiInfoBold className="size-5 cursor-pointer text-gray-500" />
 				</div>
 				<p className="text-gray-500">
-					By clicking “confirm & pay”, you agree to
+					By clicking “confirm & pay”, you agree to{' '}
 					<Link to="#" className="text-blue-700 underline decoration-blue-600">
 						Ticket’s general terms and conditions
-					</Link>
-					and
+					</Link>{' '}
+					and{' '}
 					<Link to="#" className="text-blue-700 underline decoration-blue-600">
 						cancellation policy
 					</Link>
 					.
 				</p>
-				<button type="submit" className="my-6 flex items-center justify-center gap-2 rounded-lg bg-blue-600 pb-4 pl-11 pr-11 pt-4 text-lg font-semibold text-white shadow-md sm:pb-3 sm:pl-10 sm:pr-10 sm:pt-3">
+				<button type="submit" className="my-6 flex items-center justify-center gap-2 rounded-2xl bg-blue-600 px-10 py-4 text-lg font-semibold text-white shadow-md">
 					<MdLock />
-					Confirm & Pay
+					Confirm & pay
 				</button>
 			</div>
 		</form>
--- src/sections/PaymentMode.jsx
+++ src/sections/PaymentMode.jsx
@@ -1,71 +1,19 @@
 import { IoMdCard } from 'react-icons/io'
-import { FaApple, FaGoogle } from 'react-icons/fa'
-import { CreditCardForm } from '../components'
-import { ImRadioChecked2 } from 'react-icons/im'
+import { FaApplePay, FaGooglePay } from 'react-icons/fa'
+import { CreditCardForm, PaymentOption } from '../components'
 
 const PaymentMode = () => {
 	return (
 		<section id="payment-mode" className="my-8">
 			<h6 className="mt-10 text-3xl font-medium">Select your mode of payment</h6>
 			<p className="py-5 text-xl font-light text-gray-700">Payments with Tickets are secure and encrypted.</p>
-			<label className="cursor-pointer">
-				<input name="paymentMode" className="peer sr-only" type="radio" />
-				<div className="group relative my-5 cursor-pointer rounded-2xl bg-gray-100 px-7 py-5 ring-1 ring-gray-300 peer-checked:bg-white peer-checked:ring-gray-700">
-					<div className="flex items-center gap-4">
-						<div className="flex h-8 w-10 items-center justify-center rounded-md border">
-							<IoMdCard className="text-gray-600" />
-						</div>
-						<h6 className="text-xl font-medium text-gray-500 peer-checked:group-[]:text-black">Credit & debit card</h6>
-						<ImRadioChecked2 />
-					</div>
-					{/* <CreditCardForm/> */}
-				</div>
-			</label>
-			<div className="my-5 rounded-2xl bg-gray-100 px-7 py-5 ring-1 ring-gray-300 has-[:checked]:bg-white has-[:checked]:ring-gray-700">
-				<div className="relative flex items-center gap-4">
-					<div className="flex h-8 w-10 items-center justify-center rounded-md border">
-						<IoMdCard className="text-gray-600" />
-					</div>
-					<input name="paymentMode" className="peer absolute inset-y-0 end-4" type="radio" />
-					<h6 className="text-xl font-medium text-gray-500 peer-checked:text-black">Credit & debit card</h6>
-				</div>
-				{/* <CreditCardForm/> */}
-			</div>
-			<div className="my-5 rounded-2xl bg-gray-100 px-7 py-5 ring-1 ring-gray-300 has-[:checked]:bg-white has-[:checked]:ring-gray-700">
-				<div className="relative flex items-center gap-4">
-					<div className="flex h-8 w-10 items-center justify-center rounded-md border">
-						<IoMdCard className="text-gray-600" />
-					</div>
-					<input name="paymentMode" className="peer absolute inset-y-0 end-4" type="radio" />
-					<h6 className="text-xl font-medium text-gray-500 peer-checked:text-black">Credit & debit card</h6>
-				</div>
-				{/* <CreditCardForm/> */}
-			</div>
-
-			<div className="my-5 rounded-2xl bg-gray-100 p-5 text-gray-600 ring-1 ring-gray-700">
-				<div className="flex items-center justify-between">
-					<div className="flex items-center gap-4">
-						<div className="flex h-8 w-14 items-center justify-center rounded-lg border bg-gray-200 text-gray-500">
-							<FaApple />
-							<p className="">Pay</p>
-						</div>
-						<h6 className="text-gray-500">Coming soon</h6>
-					</div>
-					<input name="paymentMode" type="radio" />
-				</div>
-			</div>
-			<div className="my-5 rounded-2xl border border-gray-300 bg-gray-100 p-5 text-gray-600">
-				<div className="flex items-center justify-between">
-					<div className="flex items-center gap-4">
-						<div className="flex h-8 w-14 items-center justify-center rounded-lg border bg-gray-200 text-gray-500">
-							<FaGoogle />
-							<p className="">Pay</p>
-						</div>
-						<h6 className="text-gray-500">Coming soon</h6>
-					</div>
-					<input name="paymentMode" type="radio" />
-				</div>
-			</div>
+			<PaymentOption name="Credit & debit card" defaultChecked icon={<IoMdCard className="size-5" />}>
+				<CreditCardForm />
+			</PaymentOption>
+			<PaymentOption name="Apple Pay" icon={<FaApplePay className="size-11" />}>
+				<CreditCardForm />
+			</PaymentOption>
+			<PaymentOption name="Google Pay" disabled icon={<FaGooglePay className="size-11" />} />
 		</section>
 	)
 }
