9ae722dcb40b0ea95b23a83bbf0a474cf5554c29
Srija Adhya <srijaadhya.11sc.2020@gmail.com>
2024-07-11 13:26:10 +0530

    fix: question section

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -4,7 +4,7 @@ import { GoPlus } from 'react-icons/go'
 const Question = ({ ques, ans }) => {
 	return (
 		<label className="cursor-pointer">
-			<input type="radio" className="peer sr-only" />
+			<input type="radio" className="peer sr-only" name='faq' />
 			<div className="group my-4 items-center gap-4 rounded-2xl p-6 ring-1 ring-gray-300 peer-checked:ring-gray-500">
 				<div className="flex items-center gap-4">
 					<GoPlus className="text-gray-500 peer-checked:group-[]:hidden" />
