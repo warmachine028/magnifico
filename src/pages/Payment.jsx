3d9c9a1605bd9d98bb0384862270d28812917231
Srija Adhya <srijaadhya.11sc.2020@gmail.com>
2024-07-29 22:08:27 +0530

    feat: SEO ready

--- src/pages/Payment.jsx
+++ src/pages/Payment.jsx
@@ -1,9 +1,15 @@
+import { Helmet } from 'react-helmet'
 import { DestinationCard } from '../components'
 import { PaymentHero, AdditionalInformation, PaymentMode, TotalPayable, FAQ } from '../sections'
 
 const Payment = () => {
 	return (
 		<main className="dark:bg-slate-900">
+			<Helmet>
+				<meta charSet="utf-8" />
+				<title>Magnifico-Payments</title>
+				<link rel="canonical" href="http://mysite.com/example" />
+			</Helmet>
 			<div className="sticky left-0 top-20 z-40 flex w-full justify-center bg-blue-600 p-3 text-center text-white sm:hidden">Holding your tickets for 30:00</div>
 			<div className="mx-auto pb-14 font-sans sm:container sm:flex sm:flex-col sm:px-14 sm:py-16">
 				<div className="px-7 pt-12 sm:flex">
