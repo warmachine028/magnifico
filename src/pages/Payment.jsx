317ed1582872c3a247ad03079db36fafe2f9f242
Srija Adhya <srijaadhya.11sc.2020@gmail.com>
2024-07-29 23:07:42 +0530

    feat: SEO ready

--- src/pages/Payment.jsx
+++ src/pages/Payment.jsx
@@ -9,6 +9,7 @@ const Payment = () => {
 				<meta charSet="utf-8" />
 				<title>Magnifico-Payments</title>
 				<link rel="canonical" href="https://magnifico-sp.vercel.app/" />
+				<meta property="og:image" content="https://github.com/user-attachments/assets/a8e90a68-36ec-4d0e-9146-3a2a07f3785d" />
 			</Helmet>
 			<div className="sticky left-0 top-20 z-40 flex w-full justify-center bg-blue-600 p-3 text-center text-white sm:hidden">Holding your tickets for 30:00</div>
 			<div className="mx-auto pb-14 font-sans sm:container sm:flex sm:flex-col sm:px-14 sm:py-16">
