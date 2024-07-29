7d1c26aa8d87decf8de47b42934374555a51a53e
Srija Adhya <srijaadhya.11sc.2020@gmail.com>
2024-07-29 23:12:05 +0530

    feat: SEO ready

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,6 +8,7 @@ const Home = () => {
 				<meta charSet="utf-8" />
 				<title>Magnifico-Home</title>
 				<link rel="canonical" href="https://magnifico-sp.vercel.app/" />
+				<meta property="og:image" content="https://github.com/user-attachments/assets/44078aaf-5da1-4809-88f1-495a6cd6823a" />
 			</Helmet>
 			<div className="mx-auto sm:container sm:px-2 xl:px-10">
 				<Hero />
