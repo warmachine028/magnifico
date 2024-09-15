d700f608a533d5e3915bd2fbb2af16668fbce4b4
warmachine028 <75939390+warmachine028@users.noreply.github.com>
2024-09-15 13:35:01 +0530

    feat: added proper Payment page Titile

--- client/src/pages/Payment.jsx
+++ client/src/pages/Payment.jsx
@@ -7,10 +7,12 @@ const Payment = () => {
 		<main className="dark:bg-slate-900">
 			<Helmet>
 				<meta charSet="utf-8" />
-				<title>Magnifico-Payments</title>
+				<title>Magnifico | Payments</title>
 				<link rel="canonical" href="https://warmachine028.github.io/magnifico/" />
 			</Helmet>
-			<div className="sticky left-0 top-20 z-40 flex w-full justify-center bg-blue-600 p-3 text-center text-white sm:hidden">Holding your tickets for 30:00</div>
+			<div className="sticky left-0 top-20 z-40 flex w-full justify-center bg-blue-600 p-3 text-center text-white sm:hidden">
+				Holding your tickets for 30:00
+			</div>
 			<div className="mx-auto pb-14 font-sans sm:container sm:flex sm:flex-col sm:px-14 sm:py-16">
 				<div className="px-7 pt-12 sm:flex">
 					<div className="sm:mx-10 sm:w-2/3">
